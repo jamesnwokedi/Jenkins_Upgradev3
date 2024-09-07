1bc4b724832028807df8a715a51d73b86b935af8
jamesnwokedi <54027812+jamesnwokedi@users.noreply.github.com>
2024-09-07 18:00:42 +0100

    upscale to 6 tests

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -28,6 +28,27 @@ public class TestGreeter {
   }
 
    @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+   @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+   @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
